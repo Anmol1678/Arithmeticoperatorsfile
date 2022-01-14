bc4c771c2f84acecab415045538f85d13fab2765
Anmol1678 <pdipak1212@yahoo.com>
2022-01-14 23:35:41 +0000

    messege

---
+++ ArithmaticOperators.java
@@ -0,0 +1,51 @@
+package main.java.org.example;
+
+public class ArithmaticOperators {
+    public static void main  (String[] args) {
+        System.out.println("Hello");
+        int A=10;
+        int B=20;
+        byte n= 127;
+        long num=35000;
+        short myNum =300;
+        int i =15000;
+        float f = 12.34f;
+        double d= 1233.2333;
+        char c = 'P';
+        boolean isJavaFun = true;
+        boolean iLikeTesting  = false;
+        String s = ("Anmol");
+        int C= -10;
+        int D = 22;
+        System.out.println(A+B);
+        System.out.println(A*B);
+        System.out.println(A/B);
+        System.out.println(A%B);
+        System.out.println(n);
+        System.out.println(num);
+        System.out.println(myNum);
+        System.out.println(i);
+        System.out.println(B+f);
+        System.out.println(d);
+        System.out.println(c);
+        System.out.println(isJavaFun);
+        System.out.println(iLikeTesting);
+        System.out.println(s);
+        System.out.println(B+n);
+        System.out.println(B+n);
+        System.out.println(num-f);
+        System.out.println(d*c);
+        System.out.println(num%myNum);
+        System.out.println(++A);
+        System.out.println(--B);
+
+
+
+
+
+    }
+
+
+
+
+}
