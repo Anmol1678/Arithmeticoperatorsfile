f1d0c6fbfb40a2b100d5f35f365ae66f650d36ea
Anmol1678 <pdipak1212@yahoo.com>
2022-01-15 01:28:23 +0000

    mynewfile

---
+++ FirstProgramme.java
@@ -0,0 +1,46 @@
+package com.myfirstprogramme;
+
+public class FirstProgramme {
+
+    public static void main(String[] args) {
+
+        System.out.println("Priyanka ");
+        int A = 20;
+        int B = 18;
+        if (A > B) {
+            System.out.println(A + " is greater than" + B);
+        } else {
+            System.out.println(A + " is less than" + B);
+        }
+
+            int age = 20;
+            if (age < 18) {
+                System.out.println("eligible.");
+            } else {
+                System.out.println("not eligible.");
+
+            }
+            // Positive,Negative and Zero program
+            int number = 0;
+            if (number > 0)
+            {
+                System.out.println(number + "is positive number.");
+            }
+            else if(number < 0) {
+                System.out.println(number + "is negative number.");
+            }
+            else {
+                System.out.println("number is " + number);
+
+                int num = 12;
+                if (num % 2 == 0) ;
+                System.out.println(num + "is an even");
+
+                }
+
+            }
+            }
+
+
+
+
